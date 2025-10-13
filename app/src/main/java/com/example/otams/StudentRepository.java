4eca8ae491e66ca0fb2dc93bcfe111f16b881364
Arsh <asing026@uottawa.ca>
2025-10-13 17:54:59 -0400

    Student registration screen repo call

---
+++ app/src/main/java/com/example/otams/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.example.otams;
+
+public class StudentRepository {
+    public void registerStudent(String first, String last, String email, String password, String phone, String program) {
+        System.out.println("Student registered: " + first + " " + last);
+    }
+}
